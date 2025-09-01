9c28da2f05033f810c225b0e70fe400e447bf565
Ahamed Mafath <ahamedmafath550@gmail.com>
2025-09-02 01:25:40 +0530

    Add Zustand and Axios, fix CORS issues

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,33 @@
+// here we can have bunch of different states and functions that we can use in our components
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+
+// create function takes a callaback function as the 1st argument where we would like to return an object. 
+// This object will be our initial state.
+export const useAuthStore = create((set) => ({ //useAuthStore: A hook that you can use in your components to access the store's state and methods.
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+
+  isCheckingAuth: true, //loading state
+
+  onlineUsers: [],
+
+  checkAuth: async() => {
+    try {
+      const res = await axiosInstance.get('/auth/check');
+      set({authUser: res.data});
+    } catch (error) {
+      console.log("Error in checkAuth: ", error);
+      set({authUser: null});
+    } finally {
+      set({isCheckingAuth: false});
+    }
+  },
+
+
+}));
