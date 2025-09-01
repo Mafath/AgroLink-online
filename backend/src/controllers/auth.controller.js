e79ecdafbe179e645b198bfe0343f291c3d21775
Ahamed Mafath <ahamedmafath550@gmail.com>
2025-09-01 11:33:46 +0530

    Integrate middleware and add update profile backend with cloudinary

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js";
 
 
 export const signup = async (req,res) => {
@@ -112,3 +113,45 @@ export const logout = (req,res) => {
 };
 
 
+export const updateProfile = async (req,res) => {
+  // here we only update the profile photo. Full name or email cant be edited according to our application
+  // to be able to update our profile pic, we need a service(cloudinary) so we can upload our images
+
+  try {
+    // when user wants to update profile image, first they need to send us
+    const {profilePic} = req.body; //aluthen danna one photo eka
+    // check which user this is
+    const userId = req.user._id;
+
+    if(!profilePic){ //aluthen danna one photo eka user dunne nttn
+      return res.status(400).json({message: "Profile pic is required"});
+    }
+
+    // if profile picture is provided, upload it to cloudinary(cloudinary is a bucket for our images)
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    // Update the user in database
+    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new:true});
+    // By default, findOneAndUpdate() returns the document as it was before update was applied. If you set new: true,
+    // findOneAndUpdate() will instead give you the object after update was applied.
+
+    res.status(200).json(updatedUser);
+
+  } catch (error) {
+    console.log("error in update profile: ",error);
+    res.status(500).json({message: "Internal server error"});
+  }
+};
+
+
+export const checkAuth = (req,res) => {
+  // We r gonna be calling this function whenever we refresh the application. And we check if the user is authenticated or not
+  // user log wela nm req.user labenw. nttn "message": "Unauthorized - No Token Provided" labenw
+  try {
+    // send the user back to the client
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller: ", error.message);
+    res.status(500).json({message: "Internal server error"});
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,12 @@ import authRoutes from './routes/auth.route.js'
 import dotenv from 'dotenv'
 dotenv.config()
 import { connectDB } from './lib/db.js'
+import cookieParser from 'cookie-parser'
 
 
 const app = express();
 app.use(express.json()); //allows to extract json data from the request body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
