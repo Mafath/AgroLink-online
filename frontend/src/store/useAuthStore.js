4d8f50e2c76f096fb50442785f5212b3d7a9aff6
Ahamed Mafath <ahamedmafath550@gmail.com>
2025-09-03 20:07:07 +0530

    implement logout function

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -44,4 +44,15 @@ export const useAuthStore = create((set) => ({ //useAuthStore: A hook that you c
   },
 
 
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+
+
 }));
